bddb2db1a642df0427a93c3328941e1af665cdb5
shadowdevcode <vijay.b.sehgal@gmail.com>
2019-09-19 12:26:05 +0530

    Added User profile api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,15 @@ class UpdateOwnProfile(permissions.BasePermission):
 
         # Instance must have an attribute named `owner`.
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow user to update their own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.user_profile.id == request.user.id
