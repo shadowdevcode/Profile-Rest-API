6726802ad88357140d9d40c07c94f1bea4d55e20
shadowdevcode <vijay.b.sehgal@gmail.com>
2019-09-19 13:27:09 +0530

    ReConfigured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_x*s=rb&lqh3-ea%nr)wy3q=h8-5s(z8#4dk*1r7w9cx$h%6cd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
